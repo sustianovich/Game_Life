c807905be1aaaa17863e0f40c33612c61b400414
sustianovich <jmsustacha@gmail.com>
2020-05-30 21:36:26 +0200

    main, cell and game_window

---
+++ game_window_class.py
@@ -0,0 +1,30 @@
+import pygame
+from cell_class import *
+vec = pygame.math.Vector2
+
+class Game_window:
+    def __init__(self, screen, x, y):
+        self.screen = screen
+        self.pos = vec(x, y)
+        self.width, self.height = 500, 500
+        self.image = pygame.Surface((self.width, self.height))
+        self.rect = self.image.get_rect()
+        self.rows = 10
+        self.cols = 10
+        self.grid = [[Cell(self.image, x, y) for x in range(self.cols)] for y in range(self.rows)]
+
+
+    def update(self):
+        self.rect.topleft = self.pos
+        for row in self.grid:
+            for cell in row:
+                cell.update()
+
+    def draw(self):
+        self.image.fill((102, 102, 102))
+        for row in self.grid:
+            for cell in row:
+                cell.draw()
+        self.screen.blit(self.image, (self.pos.x, self.pos.y))
+
+
---
+++ main.py
@@ -0,0 +1,44 @@
+import pygame
+import sys
+import numpy as np
+import time
+import sys
+from game_window_class import *
+
+
+WIDTH, HEIGHT = 800, 800
+BACKGROUND = (199, 199, 199)
+FPS = 60
+
+def get_events():
+    global running
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+def update():
+    game_window.update()
+
+def draw():
+    window.fill(BACKGROUND)
+    game_window.draw()
+
+
+pygame.init()
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+game_window = Game_window(window, 100, 180)
+
+
+running = True
+while running:
+    get_events()
+    update()
+    draw()
+    pygame.display.update()
+    clock.tick(FPS)
+pygame.quit()
+sys.exit()
+
+
